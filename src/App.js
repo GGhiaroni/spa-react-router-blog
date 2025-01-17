efd01df7dee0bd323267e0b134ba964fc5e50810
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-17 16:04:20 -0300

    Comecando a trabalhar e testar as Rotas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./paginas/Home";
+import Sobre from "./paginas/Sobre";
 
 function App() {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobremim" element={<Sobre />} />
+        <Route path="*" element={<div>Página não encontrada.</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
