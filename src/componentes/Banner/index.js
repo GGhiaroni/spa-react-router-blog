b0f7a32c2e3861129ee2ba83158045a918b4eb58
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-18 18:20:43 -0300

    finalizando o banner

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,7 +1,29 @@
+import circuloColorido from "assets/circulo_colorido.png";
+import minhaFoto from "assets/minha_foto.png";
 import "./Banner.css";
 
 const Banner = () => {
-  return <h1>Ola</h1>;
+  return (
+    <div className="banner">
+      <div className="apresentacao">
+        <h1 className="titulo">Gabriel Ghiaroni</h1>
+        <p className="paragrafo">
+          Boas vindas ao meu espaço pessoal! Eu sou Gabriel Ghiaroni, instrutor
+          de Front-end da Alura. Aqui compartilho vários conhecimentos, espero
+          que aprenda algo novo :)
+        </p>
+      </div>
+      <div className="imagens">
+        <img
+          className="circuloColorido"
+          src={circuloColorido}
+          aria-hidden={true}
+          alt="círculo colorido"
+        />
+        <img className="minhaFoto" src={minhaFoto} alt="minha foto de perfil" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
