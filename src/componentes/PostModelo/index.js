526ca37c4a6d8b9ba75bcf27b663ed06a502defa
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-19 16:04:50 -0300

    criando o componente PostModelo e seu estilo css

---
+++ src/componentes/PostModelo/index.js
@@ -0,0 +1,15 @@
+import "./PostModelo.css";
+const PostModelo = ({ fotoCapa, titulo, children }) => {
+  return (
+    <article className="postModeloContainer">
+      <div
+        className="fotoCapa"
+        style={{ backgroundImage: `url(${fotoCapa})` }}
+      ></div>
+      <h2 className="titulo">{titulo}</h2>
+      <div className="postConteudoContainer">{children}</div>
+    </article>
+  );
+};
+
+export default PostModelo;
